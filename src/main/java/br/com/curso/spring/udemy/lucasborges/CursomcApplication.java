ebc8c6e199606879e1dbf931a2044beebfa63f88
Lucas1Borp <lucas.a.b.p1@gmail.com>
2023-03-28 22:36:12 -0300

    Criando relacionamento da tabela categorias e produtos

--- src/main/java/br/com/curso/spring/udemy/lucasborges/CursomcApplication.java
+++ src/main/java/br/com/curso/spring/udemy/lucasborges/CursomcApplication.java
@@ -1,5 +1,6 @@
 package br.com.curso.spring.udemy.lucasborges;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +9,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.curso.spring.udemy.lucasborges.domain.Categoria;
+import br.com.curso.spring.udemy.lucasborges.domain.Produto;
 import br.com.curso.spring.udemy.lucasborges.repositories.CategoriaRepository;
+import br.com.curso.spring.udemy.lucasborges.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	CategoriaRepository repository;
+	@Autowired
+	ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -21,7 +26,22 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		repository.saveAll(Arrays.asList(new Categoria("Informatica"), new Categoria("Escritorio")));
+		Categoria cat1 = new Categoria("Informatica");
+		Categoria cat2 = new Categoria("Escritorio");
+
+		Produto p1 = new Produto("Computador", BigDecimal.valueOf(2000.00));
+		Produto p2 = new Produto("Impressora", BigDecimal.valueOf(800.00));
+		Produto p3 = new Produto("Mouse", BigDecimal.valueOf(80.00));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		repository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 	}
 
--- src/main/java/br/com/curso/spring/udemy/lucasborges/domain/Produto.java
+++ src/main/java/br/com/curso/spring/udemy/lucasborges/domain/Produto.java
@@ -1,14 +1,19 @@
 package br.com.curso.spring.udemy.lucasborges.domain;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,18 +24,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "prod_id")
 	private Integer id;
-
+	@Column(name = "prod_name")
 	private String nome;
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@Column(name = "prod_preco")
+	private BigDecimal preco;
+	@ManyToAny
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 
-	public Categoria(String nome) {
+	public Produto(String nome, BigDecimal valor) {
 		this.nome = nome;
+		this.preco = valor;
+
 	}
 }
